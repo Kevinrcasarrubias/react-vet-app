ae6c7f0cfa5ab8bdc7f33a5495aec79c39b01504
99162919+Kevinrcasarrubias@users.noreply.github.com <99162919+Kevinrcasarrubias@users.noreply.github.com>
2023-06-16 10:58:17 -0600

    upload files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+  purge: ["index.html", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {},
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
